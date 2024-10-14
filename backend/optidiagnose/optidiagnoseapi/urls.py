bc138884cf158f80d2b38c65cf87ec5bef768846
anras5 <anras1filip@gmail.com>
2024-10-14 13:28:46 +0200

    feat: add rest endpoints for models

--- backend/optidiagnose/optidiagnoseapi/urls.py
+++ backend/optidiagnose/optidiagnoseapi/urls.py
@@ -1,1 +1,27 @@
-urlpatterns = []
+from django.urls import path
+
+from .views import (
+    PatientListCreate,
+    PatientDetail,
+    ExaminationListCreate,
+    ExaminationDetail,
+    ScanListCreate,
+    ScanDetail,
+    NetworkDiagnosisListCreate,
+    NetworkDiagnosisDetail,
+)
+
+urlpatterns = [
+    path("patient/", PatientListCreate.as_view(), name="patient-list-create"),
+    path("patient/<uuid:patient_id>", PatientDetail.as_view(), name="patient-detail"),
+    path("examination/", ExaminationListCreate.as_view(), name="examination-list-create"),
+    path("examination/<uuid:examination_id>", ExaminationDetail.as_view(), name="examination-detail"),
+    path("scan/", ScanListCreate.as_view(), name="scan-list-create"),
+    path("scan/<uuid:scan_id>", ScanDetail.as_view(), name="scan-detail"),
+    path("network-diagnosis/", NetworkDiagnosisListCreate.as_view(), name="network-diagnosis-list-create"),
+    path(
+        "network-diagnosis/<uuid:network_diagnosis_id>",
+        NetworkDiagnosisDetail.as_view(),
+        name="network-diagnosis-detail",
+    ),
+]
---
+++ backend/optidiagnose/optidiagnoseapi/views.py
@@ -0,0 +1,48 @@
+from rest_framework import generics
+
+from .models import Patient, Examination, Scan, NetworkDiagnosis
+from .serializers import PatientSerializer, ExaminationSerializer, ScanSerializer, NetworkDiagnosisSerializer
+
+
+class PatientListCreate(generics.ListCreateAPIView):
+    serializer_class = PatientSerializer
+    queryset = Patient.objects.all()
+
+
+class PatientDetail(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = PatientSerializer
+    queryset = Patient.objects.all()
+    lookup_url_kwarg = "patient_id"
+
+
+class ExaminationListCreate(generics.ListCreateAPIView):
+    serializer_class = ExaminationSerializer
+    queryset = Examination.objects.all()
+
+
+class ExaminationDetail(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = ExaminationSerializer
+    queryset = Examination.objects.all()
+    lookup_url_kwarg = "examination_id"
+
+
+class ScanListCreate(generics.ListCreateAPIView):
+    serializer_class = ScanSerializer
+    queryset = Scan.objects.all()
+
+
+class ScanDetail(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = ScanSerializer
+    queryset = Scan.objects.all()
+    lookup_url_kwarg = "scan_id"
+
+
+class NetworkDiagnosisListCreate(generics.ListCreateAPIView):
+    serializer_class = NetworkDiagnosisSerializer
+    queryset = NetworkDiagnosis.objects.all()
+
+
+class NetworkDiagnosisDetail(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = NetworkDiagnosisSerializer
+    queryset = Scan.objects.all()
+    lookup_url_kwarg = "network_diagnosis_id"
